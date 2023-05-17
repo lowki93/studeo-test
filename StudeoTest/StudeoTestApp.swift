073e21f46f9e1b234a30fa159a7fa182e72ed4e4
lowki93 <budain.kevin@yahoo.Fr>
2023-05-17 16:18:58 +0200

    Initial Commit

---
+++ StudeoTest/StudeoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudeoTestApp.swift
+//  StudeoTest
+//
+//  Created by Kevin Budain on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudeoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
