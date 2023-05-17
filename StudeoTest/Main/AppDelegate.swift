836a2b9afbb5bb0788aa8e0d03df1e38746d3494
lowki93 <budain.kevin@yahoo.Fr>
2023-05-17 17:14:04 +0200

    add app and scene delegate

---
+++ StudeoTest/Main/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  StudeoTest
+//
+//  Created by Kevin Budain on 17/05/2023.
+//
+
+import UIKit
+//import Swinject
+
+//let container = Container().setup()
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    dump("AppDelegate")
+    return true
+  }
+
+  func application(_ application: UIApplication,
+                   configurationForConnecting connectingSceneSession: UISceneSession,
+                   options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    dump("UISceneConfiguration")
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+
+}
+
---
+++ StudeoTest/Main/SceneDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  SceneDelegate.swift
+//  StudeoTest
+//
+//  Created by Kevin Budain on 17/05/2023.
+//
+
+import SwiftUI
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+  private var navigationController: UINavigationController?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    print("scene")
+//    guard let windowScene = scene as? UIWindowScene,
+//          let controller = container.resolve(CustomHostingController<DevicesView>.self)
+//    else { return }
+
+//    window = UIWindow(windowScene: windowScene)
+//    navigationController = UINavigationController(rootViewController: controller)
+//    navigationController?.navigationBar.prefersLargeTitles = true
+//    window?.rootViewController = navigationController
+//    window?.makeKeyAndVisible()
+  }
+
+  func sceneDidDisconnect(_ scene: UIScene) { }
+
+  func sceneDidBecomeActive(_ scene: UIScene) { }
+
+  func sceneWillResignActive(_ scene: UIScene) { }
+
+  func sceneWillEnterForeground(_ scene: UIScene) { }
+
+  func sceneDidEnterBackground(_ scene: UIScene) { }
+}
+
