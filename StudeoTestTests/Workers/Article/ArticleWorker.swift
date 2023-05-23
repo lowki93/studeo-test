6ad14939742459cfcf0d24b186020e9f552ca9ef
lowki93 <budain.kevin@yahoo.Fr>
2023-05-23 03:12:14 +0200

    update UT

--- StudeoTestTests/Workers/Article/ArticleWorker.swift
+++ StudeoTestTests/Workers/Article/ArticleWorker.swift
@@ -60,105 +60,105 @@ final class ArticleWorkerTests: XCTestCase {
     XCTAssertTrue(results.contains(expectedTheNewsApiApiArticles))
   }
 
-//  func test_articles_ko_newsApi_failed() async throws {
-//    let expectedQuery = TestObjectFactory.createRandomString()
-//    let expectedPerPage = TestObjectFactory.createRandomInt()
-//    let expectedError = TestObjectFactory.TestError.dummy
-//    let expectedGnewsArticles = [TestObjectFactory.createArticle()]
-//    let expectedMediastackApiArticles = [TestObjectFactory.createArticle()]
-//
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.any(), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 3)
-//    /// We can use a captor for the `source`. We should add  an expect for each source
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.newsApi), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
-//      .andThrow(expectedError)
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.gnews), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
-//      .andReturn(expectedGnewsArticles)
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.mediastack), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
-//      .andReturn(expectedMediastackApiArticles)
-//    delegate
-//      .expect()
-//      .call(delegate.didUpdateArticles(Arg.any()), count: 2)
-//
-//    await XCTAssertAsyncThrowsError(try await worker.articles(query: expectedQuery, perPage: expectedPerPage)) { error in
-//      XCTAssertEqual(error as! TestObjectFactory.TestError, .dummy)
-//    }
-//
-//    articleNetworkService.verify()
-//    delegate.verify()
-//  }
-//
-//  func test_articles_ko_newsApi_and_gnews_failed() async throws {
-//    let expectedQuery = TestObjectFactory.createRandomString()
-//    let expectedPerPage = TestObjectFactory.createRandomInt()
-//    let expectedError = TestObjectFactory.TestError.dummy
-//    let expectedMediastackApiArticles = [TestObjectFactory.createArticle()]
-//
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.any(), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 3)
-//    /// We can use a captor for the `source`. We should add  an expect for each source
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.newsApi), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
-//      .andThrow(expectedError)
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.gnews), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
-//      .andThrow(expectedError)
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.mediastack), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
-//      .andReturn(expectedMediastackApiArticles)
-//    delegate
-//      .expect()
-//      .call(delegate.didUpdateArticles(Arg.any()), count: 1)
-//
-//    await XCTAssertAsyncThrowsError(try await worker.articles(query: expectedQuery, perPage: expectedPerPage)) { error in
-//      XCTAssertEqual(error as! TestObjectFactory.TestError, .dummy)
-//    }
-//
-//    articleNetworkService.verify()
-//    delegate.verify()
-//  }
-//
-//  func test_articles_ko_newsApi_gnews_mediastack_failed() async throws {
-//    let expectedQuery = TestObjectFactory.createRandomString()
-//    let expectedPerPage = TestObjectFactory.createRandomInt()
-//    let expectedError = TestObjectFactory.TestError.dummy
-//
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.any(), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 3)
-//    /// We can use a captor for the `source`. We should add  an expect for each source
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.newsApi), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
-//      .andThrow(expectedError)
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.gnews), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
-//      .andThrow(expectedError)
-//    try await articleNetworkService
-//      .expect()
-//      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.mediastack), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
-//      .andThrow(expectedError)
-//    delegate
-//      .reject()
-//      .call(delegate.didUpdateArticles(Arg.any()))
-//
-//    await XCTAssertAsyncThrowsError(try await worker.articles(query: expectedQuery, perPage: expectedPerPage)) { error in
-//      XCTAssertEqual(error as! TestObjectFactory.TestError, .dummy)
-//    }
-//
-//    articleNetworkService.verify()
-//    delegate.verify()
-//  }
+  func test_articles_ko_newsApi_failed() async throws {
+    let expectedQuery = TestObjectFactory.createRandomString()
+    let expectedPerPage = TestObjectFactory.createRandomInt()
+    let expectedError = TestObjectFactory.TestError.dummy
+    let expectedGnewsArticles = [TestObjectFactory.createArticle()]
+    let expectedTheNewsApiApiArticles = [TestObjectFactory.createArticle()]
+
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.any(), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 3)
+    /// We can use a captor for the `source`. We should add  an expect for each source
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.newsApi), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
+      .andThrow(expectedError)
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.gnews), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
+      .andReturn(expectedGnewsArticles)
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.theNewsApi), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
+      .andReturn(expectedTheNewsApiApiArticles)
+    delegate
+      .expect()
+      .call(delegate.didUpdateArticles(Arg.any(), type: Arg.any()), count: 2)
+
+    await XCTAssertAsyncThrowsError(try await worker.articles(query: expectedQuery, perPage: expectedPerPage)) { error in
+      XCTAssertEqual(error as! TestObjectFactory.TestError, .dummy)
+    }
+
+    articleNetworkService.verify()
+    delegate.verify()
+  }
+
+  func test_articles_ko_newsApi_and_gnews_failed() async throws {
+    let expectedQuery = TestObjectFactory.createRandomString()
+    let expectedPerPage = TestObjectFactory.createRandomInt()
+    let expectedError = TestObjectFactory.TestError.dummy
+    let expectedTheNewsApiApiArticles = [TestObjectFactory.createArticle()]
+
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.any(), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 3)
+    /// We can use a captor for the `source`. We should add  an expect for each source
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.newsApi), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
+      .andThrow(expectedError)
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.gnews), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
+      .andThrow(expectedError)
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.theNewsApi), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
+      .andReturn(expectedTheNewsApiApiArticles)
+    delegate
+      .expect()
+      .call(delegate.didUpdateArticles(Arg.any(), type: Arg.any()), count: 1)
+
+    await XCTAssertAsyncThrowsError(try await worker.articles(query: expectedQuery, perPage: expectedPerPage)) { error in
+      XCTAssertEqual(error as! TestObjectFactory.TestError, .dummy)
+    }
+
+    articleNetworkService.verify()
+    delegate.verify()
+  }
+
+  func test_articles_ko_newsApi_gnews_mediastack_failed() async throws {
+    let expectedQuery = TestObjectFactory.createRandomString()
+    let expectedPerPage = TestObjectFactory.createRandomInt()
+    let expectedError = TestObjectFactory.TestError.dummy
+
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.any(), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 3)
+    /// We can use a captor for the `source`. We should add  an expect for each source
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.newsApi), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
+      .andThrow(expectedError)
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.gnews), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
+      .andThrow(expectedError)
+    try await articleNetworkService
+      .expect()
+      .call(articleNetworkService.articles(from: Arg.eq(ArticleSource.theNewsApi), query: Arg.eq(expectedQuery), perPage: Arg.eq(expectedPerPage)), count: 1)
+      .andThrow(expectedError)
+    delegate
+      .reject()
+      .call(delegate.didUpdateArticles(Arg.any(), type: Arg.any()))
+
+    await XCTAssertAsyncThrowsError(try await worker.articles(query: expectedQuery, perPage: expectedPerPage)) { error in
+      XCTAssertEqual(error as! TestObjectFactory.TestError, .dummy)
+    }
+
+    articleNetworkService.verify()
+    delegate.verify()
+  }
 }
